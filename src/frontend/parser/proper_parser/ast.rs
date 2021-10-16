17377ca10ef09fad592cf9346d6a84fd016995ae
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-10-16 08:37:43 -0400

    Prepare the ast types for use by types.rs.

--- src/frontend/parser/proper_parser/ast.rs
+++ src/frontend/parser/proper_parser/ast.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::borrow::Cow;
 use logos::Span;
 
 #[derive(Debug)]
@@ -8,10 +9,12 @@ pub enum TopLevelAstNode<'a> {
     Export(Span, Box<TopLevelAstNode<'a>>),
     ExportDefault(Span, Box<TopLevelAstNode<'a>>),
 
-    EnumDec(Span, String, EnumDecAstNode),
+    EnumDec(Span, String, EnumDecAstNode<'a>),
 
-    StructDec(Span, String, StructDecAstNode),
+    StructDec(Span, String, StructDecAstNode<'a>),
+    ClassDec(Span, String, ClassDecAstNode<'a>),
 
+    TypeAlias(Span, AstType<'a>, AstType<'a>),
 
     CommentedNode(Span, &'a str, Box<TopLevelAstNode<'a>>),
 
@@ -21,33 +24,57 @@ pub enum TopLevelAstNode<'a> {
 impl<'comment_contents> TopLevelAstNode<'comment_contents> {
     pub fn get_span(&self) -> Span {
         match self {
+            Self::ClassDec(span, _, _) |
             Self::CommentedNode(span, _, _) |
             Self::EnumDec(span, _, _) |
             Self::Export(span, _) |
             Self::ExportDefault(span, _) |
             Self::ImportFrom(span, _, _) |
-            Self::StructDec(span, _, _) => span.clone(),
+            Self::StructDec(span, _, _) |
+            Self::TypeAlias(span, _, _) => span.clone(),
             Self::Empty => unimplemented!("Can't get the span of an empty AST node.")
         }
     }
 }
 
+pub type Generics<'a> = (Span, HashMap<Cow<'a, str>, (Span, Option<AstType<'a>>)>);
+
 #[derive(Debug)]
-pub struct EnumDecAstNode {
-    generics: HashMap<String, (Span, AstType)>,
-    values: HashMap<String, (Span, Vec<AstType>)>,
+pub struct EnumDecAstNode<'a> {
+    generics: Generics<'a>,
+    values: HashMap<String, (Span, Vec<AstType<'a>>)>,
 }
 
 #[derive(Debug)]
-pub struct StructDecAstNode {
-    generics: HashMap<String, (Span, AstType)>,
-
-    properties: HashMap<String, AstType>,
+pub struct StructDecAstNode<'a> {
+    generics: Generics<'a>,
+    implements: Vec<AstType<'a>>,
+    properties: HashMap<String, AstType<'a>>,
 }
 
 #[derive(Debug)]
-pub enum AstType {
+pub struct ClassDecAstNode<'a> {
+    generics: Generics<'a>,
+    implements: Vec<AstType<'a>>,
+    extends: Option<AstType<'a>>,
+    properties: HashMap<String, AstType<'a>>,
+}
 
+#[derive(Debug)]
+pub enum AstType<'a> {
+    RootName(Span, &'a str),
+    MemberOf(Span, Box<AstType<'a>>, &'a str),
+    GenericOf(Span, Box<AstType<'a>>, Generics<'a>),
+}
+
+impl<'a> AstType<'a> {
+    pub fn get_span(&self) -> Span {
+        match self {
+            Self::RootName(span, _) |
+            Self::MemberOf(span, _, _) |
+            Self::GenericOf(span, _, _) => span.clone(),
+        }
+    }
 }
 
 #[derive(Debug)]
